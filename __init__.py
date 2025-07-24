303f1333a1aaac2bd7e9a6733a682cb787e672f6
Shin Asakawa <10644967+ShinAsakawa@users.noreply.github.com>
2025-07-24 23:50:04 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -9,5 +9,5 @@ __copyright__ = 'Copyright 2025 {0}'.format(__author__)
 from .tokenizers import *
 from .models import direct_TLA, indirect_TLA, combined_TLA, Seq2Seq_wAtt, Seq2Seq_woAtt
 from .psylex71 import Psylex71_Dataset
-#from .transformer import Transformer
+from .transformer import Transformer
 from .utils import fit_an_epoch, eval_an_epoch  
